37bd5b85adf82ca3508eb7ed99d9ddfad1206d4d
topoescobar <ivan90ge@gmail.com>
2023-09-08 23:17:44 -0300

    query

--- models/PlaceModel.js
+++ models/PlaceModel.js
@@ -1,13 +1,13 @@
 import mongoose, { Model, Schema, model } from 'mongoose'
 
-const placeSchema = Schema({
+const placeSchema = new mongoose.Schema({
   title: { type: String, required: true },
   imgUrl: { type: String, required: true },
   description: { type: String, required: false },
   alt: { type: String, required: false },
-  category: { type: Schema.Types.ObjectId, ref: 'category', required: true } //ref para usar populate desde el controlelr
+  category: { type: mongoose.Types.ObjectId, ref: 'category', required: true } //ref para usar populate desde el controlelr
 }, { timestamps: true }) //segundo parametro opcional
 
-const PlaceModel = model('places', placeSchema)
+const PlaceModel = model('place', placeSchema)
 
 export default PlaceModel
